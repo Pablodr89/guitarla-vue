bc09c9b06bffe7d61a78610852cb8bf55e0c41f7
Pablodr89 <padoro89@gmail.com>
2023-08-03 13:36:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup>
+    import {ref, reactive, onMounted} from 'vue'
+    import {db} from './data/guitarras'
+    import Guitarra from './components/Guitarra.vue'
+    import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
+
+    //Reactive, solo para objetos que se relacionen
+    // let state = reactive({
+    //     guitarras: db
+    // })
+
+    // console.log(state.guitarras)
+
+    //Ref
+    let guitarras = ref([])
+    let carrito = ref([])
+    let guitarra = ref({})
+
+    onMounted(() => {
+        guitarras.value = db
+        guitarra.value = db[3]
+    })
+
+    let agregarCarrito = (guitarra) => {
+        let existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+        if(existeCarrito >= 0) {
+            carrito.value[existeCarrito].cantidad++
+        } else {
+            guitarra.cantidad = 1
+            carrito.value.push(guitarra)
+        }
+    }
+
+    let disminuirCantidad = (id) => {
+        let index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad <= 1) return
+        carrito.value[index].cantidad--
+    }
+
+    let aumentarCantidad = (id) => {
+        let index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad >= 5) return
+        carrito.value[index].cantidad++
+    }
+</script>
+
+<template>
+    <Header 
+        :carrito="carrito"
+        :guitarra="guitarra"
+        @disminuir-cantidad="disminuirCantidad"
+        @aumentar-cantidad="aumentarCantidad"
+        @agregar-carrito="agregarCarrito"
+    />
+
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+            <Guitarra 
+                v-for="guitarra in guitarras"
+                :guitarra="guitarra"
+                @agregar-carrito="agregarCarrito"
+            />
+        </div>
+    </main>
+
+    <Footer />
+</template>
+
+<style scoped>
+
+</style>
